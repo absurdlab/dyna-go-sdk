77a5d13ab42b3e6d5b89fcc509c510d94f19b26b
Weinan Qiu <davidiamyou@gmail.com>
2021-01-02 09:18:55 +0800

    add errcode subject not found

--- idp/errcode/codes.go
+++ idp/errcode/codes.go
@@ -4,5 +4,6 @@ const (
 	BadPassword      = "bad_password"
 	ExpiredPassword  = "expired_password"
 	UsernameNotFound = "username_not_found"
+	SubjectNotFound  = "subject_not_found"
 	Internal         = "internal"
 )
