3cfc80a207d1266ac7c2d43e2197f3c6ef727746
Weinan Qiu <davidiamyou@gmail.com>
2021-01-02 09:24:15 +0800

    add errcode amr_not_supported

--- idp/errcode/codes.go
+++ idp/errcode/codes.go
@@ -5,5 +5,6 @@ const (
 	ExpiredPassword  = "expired_password"
 	UsernameNotFound = "username_not_found"
 	SubjectNotFound  = "subject_not_found"
+	AmrNotSupported  = "amr_not_supported"
 	Internal         = "internal"
 )
